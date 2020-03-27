d885f45fb0e26d60f56bd9170bcf095713a8f4a2
Thiago Krempser <tkrempser@gmail.com>
2020-03-27 20:14:28 -0300

    Fix typo

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,5 +8,5 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_lengt=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
