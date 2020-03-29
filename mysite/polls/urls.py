5762f7d6db744ad42cdf55e28ee3f4e76088eeb5
Thiago Krempser <tkrempser@gmail.com>
2020-03-29 17:59:25 -0300

    Minor updates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),  # /polls/
-    path('<int:question_id>/', views.detail, name='detail'),  # /polls/5/
-    path('<int:question_id>/results/', views.results, name='results'),  # /polls/5/results/
-    path('<int:question_id>/vote/', views.vote, name='vote'),  # /polls/5/vote/
+    path('', views.index, name='index'),  # /
+    path('<int:question_id>/', views.detail, name='detail'),  # /5/
+    path('<int:question_id>/results/', views.results, name='results'),  # /5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # /5/vote/
 ]
